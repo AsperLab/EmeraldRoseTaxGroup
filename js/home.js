97a23fe89c139e3bd80c40bae135c2696768b14a
akhilcjacob <akhilcjacob@gmail.com>
2018-01-08 20:28:03 -0500

    changed colors

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     particleground(document.getElementById('particles'), {
-        dotColor: '#5cbdaa',
-        lineColor: '#5cbdaa'
+        dotColor: '#fff',
+        lineColor: '#fff'
     });
     var intro = document.getElementById('intro');
     intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
