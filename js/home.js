554fbb5e427617e82f37fb763e394564c44b7f6f
akhilcjacob <akhilcjacob@gmail.com>
2018-01-08 22:26:37 -0500

    some minor ui changes

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     particleground(document.getElementById('particles'), {
-        dotColor: '#fff',
-        lineColor: '#fff'
+        dotColor: '#9E9E9E',
+        lineColor: '#9E9E9E'
     });
     var intro = document.getElementById('intro');
     intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
