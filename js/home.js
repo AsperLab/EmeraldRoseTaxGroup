9a71dd6092f69d929db9bffe0148e607bc9235cb
akhilcjacob <akhilcjacob@gmail.com>
2018-01-12 00:39:55 -0500

    Boostrap rewrite

--- js/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    particleground(document.getElementById('particles'), {
-        dotColor: '#9E9E9E',
-        lineColor: '#9E9E9E'
-    });
-    var intro = document.getElementById('intro');
-    intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
-}, false);
